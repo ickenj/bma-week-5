8d3a7dde7c8dd8ef7b57fc40e5e957b6c242a02e
Joanne Icken <ickenj@gmail.com>
2012-10-01 20:42:26 -0400

    added Assignment.java

---
+++ src/Assignment.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author student
+ *
+ */
+public class Assignment {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
